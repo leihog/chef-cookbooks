bc3bf3df650b6511105edfc30dc28325735c46f2
Leif Högberg <leihog@gmail.com>
2011-10-24 14:38:34 +0200

    properly added another option.

--- locale/attributes/default.rb
+++ locale/attributes/default.rb
@@ -3,5 +3,4 @@
 #
 # Copyleft 2011 Leif Högberg <leihog@gmail.com>
 #
-default.locale[:locales] = ["en_GB.ISO-8859-15 ISO-8859-15"]
-default.locale[:locales] = ["en_GB.UTF-8 UTF-8"]
+default.locale[:locales] = [ "en_GB.ISO-8859-15 ISO-8859-15", "en_GB.UTF-8 UTF-8" ]
